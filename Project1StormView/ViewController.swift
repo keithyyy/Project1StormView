587a412c3f0259acf2a72bbd2e8576f3730d5c52
Keith Crooc <keith.crooc@gmail.com>
2021-04-04 23:21:27 -0400

    Initial Commit

---
+++ Project1StormView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1StormView
+//
+//  Created by Keith Crooc on 2021-04-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
